0f4ec891572131dab4e403de350fcf68a949c2e3
pzhbao <pzhbao@cn.ibm.com>
2018-10-14 13:03:46 +0800

    add categoryService

---
+++ src/main/java/com/imooc/service/impl/BuyerServiceImpl.java
@@ -0,0 +1,11 @@
+package com.imooc.service.impl;
+
+/**
+ * 描述:
+ * ${DESCRIPTION}
+ *
+ * @author pzb
+ * @create 2018-10-14 2:47 AM
+ */
+public class BuyerServiceImpl {
+}
---
+++ src/main/java/com/imooc/service/impl/SellerServiceImpl.java
@@ -0,0 +1,10 @@
+package com.imooc.service.impl;
+
+/**
+ * 描述:
+ *
+ * @author pzb
+ * @create 2018-10-14 2:54 AM
+ */
+public class SellerServiceImpl {
+}
